4f10884f1c2d5caf43a493ec62c0da8012f6c790
digvijay sinh rathod <155974214+digvijaysinhx1@users.noreply.github.com>
2025-10-30 17:35:53 +0530

    landing-page-in-progress

--- Projects/Landing-page/script.js
+++ Projects/Landing-page/script.js
@@ -5,3 +5,21 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     hamLinks.classList.toggle('show');
 });
+
+
+
+// function hide() {
+//     const img = document.getElementsByClassName('octa-sec-img2')
+    
+//     if(window.innerWidth <=766){
+//         img.style.display = 'none'
+//     }
+//     else{
+//         img.style.display = 'flex'
+//     }
+
+// }
+
+// hide();
+
+// window.addEventListener('resize', hide)
--- index.js
+++
@@ -1,9 +0,0 @@
-function mul(x){
-    return function multi(y){
-        return x * y;
-    }
-}
-
-let multiply = mul(2)
-
-console.log(multiply(5))
